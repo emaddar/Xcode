67a65cd31d1c965c00965bccc7e3242c8657107f
emaddar <108935365+emaddar@users.noreply.github.com>
2022-09-13 15:33:02 +0200

    Add files via upload

---
+++ pages/2.8 Collection Types.py
@@ -0,0 +1,148 @@
+# Contents of ~/my_app/streamlit_app.py
+from cProfile import label
+import streamlit as st
+
+st.set_page_config(
+    page_icon=":blue_heart:",
+)
+
+
+st.markdown("""
+# Collection Types
+
+Containers that can store multiple values Like a “group of variables“
+Structures for ordered and unordered groups of values
+- Set is an unordered collection of unique values
+- Array is an ordered collection of values accessible by index
+- Dictionary is an unordered collection of key-value associations
+
+
+
+## Set
+""")
+
+code = """
+import Foundation
+// Declaring an unordered collection of unique elements
+var cakeIngredients: Set = ["Eggs", "Butter", "Flour", "Milk"]
+// Showing the content
+print(cakeIngredients)    // "Milk", "Butter", "Flour", "Eggs"
+// Inserting a new element
+cakeIngredients.insert("Milk")
+// Showing the content
+print(cakeIngredients)    // "Milk", "Butter", "Flour", "Eggs"
+"""
+
+st.code(code, language='swift')
+
+
+
+
+
+
+
+
+
+
+
+st.markdown("""
+## Array
+#### Ways to declare an array
+""")
+
+code = """
+var tracks = ["Interlagos", "Velocitta", "Auto Club Speedway"]
+"""
+
+st.code(code, language='swift')
+
+
+code = """
+var tracks: [String] = ["Interlagos", "Velocitta", "Auto Club Speedway”]
+"""
+
+st.code(code, language='swift')
+
+
+code = """
+var tracks: Array<String> = ["Interlagos", "Velocitta", "Auto Club Speedway”]
+"""
+
+st.code(code, language='swift')
+
+
+st.markdown("""
+Example
+""")
+code = """
+
+// Declaring an ordered collection
+var visitedContries = ["Brazil", "USA", "Sweden", "Colombia"]
+// Accessing an element using its index
+print(visitedContries[0])         //"Brazil"
+// Appending an element to the collection
+visitedContries.append("Italy")
+// Inserting an element at a specific index
+visitedContries.insert("Indonesia", at: 5)
+// Getting the number of elements
+visitedContries.count             //6
+"""
+
+st.code(code, language='swift')
+
+
+
+
+
+
+
+
+
+st.markdown("""
+## Dictionary
+#### Ways to declare an array
+""")
+
+code = """
+var airports = ["CGH":"São Paulo", "CWB":"Curitiba"]
+"""
+
+st.code(code, language='swift')
+
+
+
+
+code = """
+var airports: [String:String] = ["CGH":"São Paulo", "CWB":"Curitiba"]
+"""
+
+st.code(code, language='swift')
+
+
+
+code = """
+var airports: Dictionary<String,String> = ["CGH":"São Paulo", "CWB":"Curitiba"]
+"""
+
+st.code(code, language='swift')
+
+
+
+
+
+
+st.markdown("""
+Example
+""")
+code = """
+// Declaring an unordered collection
+var countryCode = ["BR":55, "US":1, "IT":39, "IN":62]
+// Accessing an element using its key
+countryCode["IT"]            // 39
+// Adding an element to the collection
+countryCode["UK"] = 44
+// Printing the collection
+print(countryCode)          // ["IT": 39, "BR": 55, "US": 1, "UK": 44, "IN": 62]
+"""
+
+st.code(code, language='swift')
---
+++ pages/2.9 2.2 Control transfer.py
@@ -0,0 +1,171 @@
+# Contents of ~/my_app/streamlit_app.py
+from cProfile import label
+import streamlit as st
+
+st.set_page_config(
+    page_icon=":blue_heart:",
+)
+
+
+st.markdown("<h1 style='text-align: center; color: black;'>Lesson 2.3</h1>", unsafe_allow_html=True)
+
+st.markdown("""
+# The for-in Loop
+
+Use the `for-in loop` to iterate over a sequence, 
+such as ranges of numbers, items in an array, or characters in a string.
+The following example prints the first few entries in the five-times-table:
+""")
+code = """
+for index in 1...5 {
+    print("\(index) times 5 is \(index * 5) ")
+}
+//1 times 5 is 5 
+//2 times 5 is 10 
+//3 times 5 is 15 
+//4 times 5 is 20 
+//5 times 5 is 25
+"""
+
+st.code(code, language='swift')
+
+st.markdown("""
+The index variable is set at the first value in the range (1). 
+The statements within the for loop are then executed in sequence, through the final item in the range (5).
+""")
+
+
+
+st.markdown("""
+# The while Loop
+
+A `while` loop performs a set of statements until a condition becomes false.
+ These kinds of loops are best used when the number of iterations is not known before the first iteration begins.
+while evaluates its condition at the start of each pass through the loop.
+The while loop is demonstrated in the example below:
+""")
+code = """
+while a < b {
+    print(a)
+    a += 1
+}
+"""
+
+st.code(code, language='swift')
+
+st.markdown("""
+The code will execute until the a+=1 statement renders `a < b` as `false`.
+""")
+
+
+
+
+
+st.markdown("""
+# Repeat-While
+
+The `repeat-while` loop is the alternate while loop. 
+It first makes a single pass through the loop block, 
+then considers the loop's condition, and repeats the loop until the condition shows as false.
+""")
+
+
+
+
+
+code = """
+repeat {
+    x -= 1
+} while  x > 0
+"""
+
+st.code(code, language='swift')
+
+
+st.markdown("""
+Swift's repeat-while loop is similar to a do-while loop in other languages.
+""")
+
+
+
+
+
+
+
+st.markdown("""
+# Break
+
+Use the **break** statement to immediately end the execution of an entire control flow statement. 
+Also, the break statement is used within a switch statement or a loop statement to terminate 
+its execution sooner than would otherwise be the case.
+
+
+##### Break in a Loop Statement
+
+When a break statement is used within a loop statement, the loop's execution immediately stops. 
+Control transfers to the first line of code following the loop's closing brace (}). The current iteration's remaining code is skipped, and no further iterations of the loop are initiated.
+For example, you can have a loop that breaks out when the value of **a** becomes less than that of **b**:
+
+
+""")
+
+code = """
+var b = 7
+var a = 10
+while a > 0 {
+     if (a<b) {
+         break
+    } 
+    print(a) 
+    a -= 1
+}
+//10
+//9
+//8
+//7
+"""
+
+st.code(code, language='swift')
+
+st.markdown("""
+##### Break in a Switch Statement
+
+A **break** causes a switch statement to end its execution immediately, 
+and transfers control to the first line of code that follows the switch statement's closing brace (}).
+""")
+
+code = """
+var a = 5 
+var letter = "X"
+
+switch a {
+    case 1:
+     letter = "A" 
+    case 2 : 
+     letter = "B" 
+    default : 
+     break 
+}
+"""
+
+st.code(code, language='swift')
+
+st.markdown("""
+This example breaks out of the switch statement as soon as the default case is matched.
+
+**_Always use a break statement to ignore a switch case._**
+""")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
