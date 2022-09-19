c3529b968a7c6b967a8869a1fd4db01a5a4173d0
emaddar <e.a.darwich@gmail.com>
2022-09-19 13:40:40 +0200

    sfjvhksfhvksf

--- pages/4.3 Extraction de vues.py
+++ pages/4.3 Extraction de vues.py
@@ -11,7 +11,7 @@ st.set_page_config(
 st.markdown("""
 # 
 
-#### Example 1: : Change background color :heart: :green_heart: :blue_heart:
+#### Example 1 : Change background color :heart: :green_heart: :blue_heart:
 
 """)
 from PIL import Image
@@ -59,6 +59,100 @@ if st.button('Show code'):
             }
         }
     }
+        """
+    st.code(code, language='swift')
+
+st.markdown("""
+---
+""")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+st.markdown("""
+# 
+
+#### Example 2 : click to print icon
+
+""")
+from PIL import Image
+image = Image.open('Simple10.png')
+st.image(image, width=700)
+
+if st.button('Show code'):
+    code = """
+
+struct ContentView: View {
+    @State var ImageString: String = ""
+            var body: some View {
+                VStack {
+                    Image(systemName: ImageString)
+                        .foregroundColor(.black)
+                        .font(.system(size: 155))
+                    HStack {
+                        ExtButton(fond: .red, myImageB: "hifispeaker.2", ImageString: $ImageString)
+                        ExtButton(fond: .blue, myImageB: "speaker.wave.3.fill", ImageString: $ImageString)
+                        ExtButton(fond: .red, myImageB: "speaker.zzz.fill", ImageString: $ImageString)
+                        ExtButton(fond: .blue, myImageB: "speaker.slash", ImageString: $ImageString)
+                    }
+                }
+
+            }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+struct ExtButton : View{
+    var fond: Color
+    var myImageB: String
+    @Binding var ImageString: String
+
+    var body: some View{
+        Button {
+             ImageString = myImageB
+        } label: {
+            HStack {
+                Image(systemName: myImageB)
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(fond)
+            .cornerRadius(10)
+        }
+    }
+}
+
         """
     st.code(code, language='swift')
 
