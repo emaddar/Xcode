1542a832b630084e71e7e3e044383f7f0eb8c520
emaddar <e.a.darwich@gmail.com>
2022-09-20 15:23:33 +0200

    add TabView

---
+++ pages/TabView.py
@@ -0,0 +1,59 @@
+# Contents of ~/my_app/streamlit_app.py
+from cProfile import label
+from ctypes import sizeof
+import streamlit as st
+
+st.set_page_config(
+    page_icon=":apple:",
+)
+
+
+st.markdown("""
+# TabView
+
+#### Example : David 
+
+""")
+from PIL import Image
+image = Image.open('flags.png')
+st.image(image, width=500)
+
+if st.button('Show code'):
+    code = """
+    struct ContentView: View {
+    var body: some View {
+        TabView{
+            RGB()
+            //view préalablement créée
+                .tabItem {
+                    Label("Color", systemImage: "eyedropper")
+                }
+            Icons()
+                .tabItem {
+                               Label("Volume", systemImage: "speaker.fill")
+                           }
+            SliderView()
+                .tabItem {
+                            Label("Slider", systemImage: "thermometer.snowflake")
+                }
+
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+        """
+    st.code(code, language='swift')
+
+st.markdown("""
+---
+
+ 
+""")
+
+
+
