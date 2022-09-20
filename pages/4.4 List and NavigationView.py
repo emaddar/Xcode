3be42e6125f6cb43b626eebf8158a1532aa1faa1
emaddar <e.a.darwich@gmail.com>
2022-09-20 11:43:07 +0200

    add list photo

--- pages/4.4 List and NavigationView.py
+++ pages/4.4 List and NavigationView.py
@@ -14,48 +14,87 @@ st.markdown("""
 #### Example 1 : 
 
 """)
-#from PIL import Image
-#image = Image.open('Simple4.png')
-#st.image(image, width=700)
+from PIL import Image
+image = Image.open('list.png')
+st.image(image, width=500)
 
 if st.button('Show code'):
     code = """
-        struct Weather: Identifiable{
-            var id = UUID()
-            var name: String
-            var icon: String
+    import SwiftUI
+
+    struct Weather: Identifiable{
+        var id = UUID()
+        var name: String
+        var icon: String
+    }
+
+    var weathers = [
+        Weather(name: "sun", icon: "sun.max.fill"),
+        Weather(name: "cloud", icon: "smoke.fill"),
+        Weather(name: "storm", icon: "cloud.bolt.fill")
+    ]
+
+
+    struct WeatherView: View {
+        let name: String
+
+        var body: some View {
+            Text("Selected weather: \(name)")
+                .font(.largeTitle)
         }
-
-        var weathers = [
-            Weather(name: "sun", icon: "sun.max.fill"),
-            Weather(name: "cloud", icon: "smoke.fill"),
-            Weather(name: "storm", icon: "cloud.bolt.fill")
-        ]
+    }
 
 
-        struct ExoWeather: View {
-            var body: some View {
-                    List(weathers) { weather in
-                            ExtractedView(weather: weather)
-                    }
-            }
-        }
 
-        struct ExoWeather_Previews: PreviewProvider {
-            static var previews: some View {
-                ExoWeather()
-            }
-        }
-
-        struct ExtractedView: View {
-            var weather: Weather
-            var body: some View {
-                HStack{
-                    Image(systemName: weather.icon)
-                    /*@START_MENU_TOKEN@*/Text(weather.name)/*@END_MENU_TOKEN@*/
+    struct ExoWeather: View {
+        var body: some View {
+            NavigationView{
+                List(weathers) { weather in
+                    NavigationLink(destination: ExtractedView1(weather: weather),
+                                    label: {ExtractedView(weather: weather)}
+                    
+                    )
+    //                    ExtractedView(weather: weather)
                 }
+                .navigationTitle("Weather")
             }
         }
+    }
+
+    struct ExoWeather_Previews: PreviewProvider {
+        static var previews: some View {
+            ExoWeather()
+        }
+    }
+
+    struct ExtractedView: View {
+        var weather: Weather
+        var body: some View {
+            HStack{
+                Image(systemName: weather.icon)
+                Text(weather.name)
+            }
+        }
+    }
+
+
+
+    // nouvelle écran
+    struct ExtractedView1: View {
+        var weather: Weather
+        var body: some View {
+            VStack{
+                Text("Today is : \(Date(), style: .date)   \(Date(), style: .time)")
+                    .padding(50)
+                Image(systemName: weather.icon)
+                    .font(.system(size: 155))
+                Text("Weather : \(weather.name)")
+            }
+        }
+    }
+
+
+
         """
     st.code(code, language='swift')
 
